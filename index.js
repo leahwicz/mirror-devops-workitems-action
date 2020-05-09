f98cf36a5d1c30cd127b63ba7f6c77963c9cc487
Leah Antkiewicz <laa018@bucknell.edu>
2020-05-08 21:42:52 -0400

    remove async

--- index.js
+++ index.js
@@ -5,24 +5,22 @@ const azdev = require('azure-devops-node-api');
 const azpPAT = process.env['AZP_PAT'];
 
 // Gets the node api for getting workitems
-async function getNodeApi() {
+function getNodeApi() {
     let authHandler = azdev.getPersonalAccessTokenHandler(azpPAT); 
     let connection = new azdev.WebApi('https://dev.azure.com/mseng', authHandler);  
     return await connection.getWorkItemTrackingApi();
 }
 
-async function run() {
-    try {
-        // get the area path
-        const areaPath = core.getInput('area-path');
-        console.log(`Area path to look for work items under ${areaPath}`);
+try {
+    // get the area path
+    const areaPath = core.getInput('area-path');
+    console.log(`Area path to look for work items under ${areaPath}`);
 
-        const nodeApi = await getNodeApi();
-        const wiql = `SELECT System.ID from workitems where [System.AreaPath] = '${areaPath}' AND [System.State] = 'Active'`;
-        const items = await nodeApi.queryByWiql({query: wiql});
+    const nodeApi = getNodeApi();
+    const wiql = `SELECT System.ID from workitems where [System.AreaPath] = '${areaPath}' AND [System.State] = 'Active'`;
+    const items = await nodeApi.queryByWiql({query: wiql});
 
 
-    } catch (error) {
-        core.setFailed(error.message);
-    }
+} catch (error) {
+    core.setFailed(error.message);
 }
