1fca152e96b689b4c3458ccda7574cc4eeb1a7cb
Leah Antkiewicz <laa018@bucknell.edu>
2020-05-08 21:35:28 -0400

    Adding ADO API dependencies

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const azdev = require('azure-devops-node-api');
+
+const azpPAT = process.env['AZP_PAT'];
+
+// Gets the node api for getting workitems
+async function getNodeApi() {
+    let authHandler = azdev.getPersonalAccessTokenHandler(azpPAT); 
+    let connection = new azdev.WebApi('https://dev.azure.com/mseng', authHandler);  
+    return await connection.getWorkItemTrackingApi();
+}
 
 try {
     // get the area path
     const areaPath = core.getInput('area-path');
-    console.log(`Hello ${areaPath}!`);
+    console.log(`Area path to look for work items under ${areaPath}`);
+
+    const nodeApi = await getNodeApi();
+    const wiql = `SELECT System.ID from workitems where [System.AreaPath] = '${areaPath}' AND [System.State] = 'Active'`;
+    const items = await nodeApi.queryByWiql({query: wiql});
+
 
   } catch (error) {
     core.setFailed(error.message);
