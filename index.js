c34991e99aac26b95ed783cd3a8edbd7e88920d9
Leah Antkiewicz <laa018@bucknell.edu>
2020-05-15 21:03:14 -0400

    Updating task to make it more generic

--- index.js
+++ index.js
@@ -2,25 +2,98 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const azdev = require('azure-devops-node-api');
 
-const azpPAT = process.env['AZP_PAT'];
-
 // Gets the node api for getting workitems
-function getNodeApi() {
-    let authHandler = azdev.getPersonalAccessTokenHandler(azpPAT); 
-    let connection = new azdev.WebApi('https://dev.azure.com/mseng', authHandler);  
+async function getNodeApi(azpToken, azpOrg) {
+    let authHandler = azdev.getPersonalAccessTokenHandler(azpToken); 
+    let connection = new azdev.WebApi('https://dev.azure.com/' + azpOrg, authHandler);  
     return await connection.getWorkItemTrackingApi();
 }
 
-try {
-    // get the area path
-    const areaPath = core.getInput('area-path');
-    console.log(`Area path to look for work items under ${areaPath}`);
+async function getWorkItems(azpPAT, azpOrg, areaPath, workItemType) {
+    console.log('Getting existing Azure DevOps work items...');
+    // query by WIQL to get work item ids that match criteria
+    const nodeApi = await getNodeApi(azpPAT, azpOrg);
+    const filterOnType = workItemType ? `[System.WorkItemType] = '${workItemType}' AND` : ``
+    const wiql = `SELECT [System.Id], [System.WorkItemType], [System.Title], [System.State] from workitems where ${filterOnType} [System.AreaPath] UNDER 'AzureDevOps\\VSTS\\Pipelines\\Akvelon Pipelines Task Team' AND [System.State] = '5 - PG Engaged'`;
+    const result = await nodeApi.queryByWiql({query: wiql});
 
-    const nodeApi = getNodeApi();
-    const wiql = `SELECT System.ID from workitems where [System.AreaPath] = '${areaPath}' AND [System.State] = 'Active'`;
-    const items = await nodeApi.queryByWiql({query: wiql});
+    const count = result['workItems'].length;
+    console.log(`Azure DevOps work items that match: ${count}\n`);
 
+    var workItemIds = result.workItems.map(function (wi) { return wi.id; });
+    let items = await nodeApi.getWorkItems(workItemIds, ["System.Id", "System.WorkItemType", "System.Title"]);
 
-} catch (error) {
-    core.setFailed(error.message);
+    return items;
 }
+
+async function getExistingIssues(octokit, labelFilter, issuePrefix) {
+    console.log('Getting existing GitHub issues...');
+    const options = octokit.issues.listForRepo.endpoint.merge({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        state: 'open',
+        labels: labelFilter
+    });
+
+    const issuesAndPulls = await octokit.paginate(options);
+
+    return issuesAndPulls.filter((value) => {
+        return !value.pull_request && value.title?.startsWith(issuePrefix);
+    }).map(function(issue) { return issue.title; });
+}
+
+async function createIssues(octokit, existingIssues, workItems, labelForIssues, issuePrefix) {
+    console.log(`GitHub issues that match: ${existingIssues.length}\n`);
+
+    workItems.forEach(item => {
+        var str = JSON.stringify(item, null, 4); 
+        console.log(str);
+
+        if (existingIssues.some(v => v.includes(`${item.id}`))) {
+            console.log("Work item '" + item.id + "' already has an issue created for it");
+        } else {
+            console.log("Creating issue for work item '" + item.id + "'");
+
+            // Create bug info
+            const prefix = issuePrefix ? issuePrefix : `[Work item`
+            const title = `${prefix} ${item.id}] ${item.fields['System.Title']}`;
+            const url = `${item.url}`.replace("_apis/wit/workItems", "_workitems/edit");
+            const description = `Please look at work item ${item.id} that has been opened here:\n${url}`
+            console.log("Work item URL '" + description + "'" + title);
+
+            octokit.issues.create({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                title: title,
+                body: description,
+                labels: labelForIssues.split(',')
+            });
+        }
+    });
+}
+
+async function run()
+{
+    try {
+        // get inputs
+        const azpPAT = core.getInput('ado-token', { required: true });
+        const githubPAT = core.getInput('repo-token', { required: true });
+        const azpOrg = core.getInput('org-name', { required: true });
+        const areaPath = core.getInput('area-path', { required: true });
+        const labelFilter = core.getInput('label-filter', { required: false });
+        const labelForIssues = core.getInput('labels-for-issues', { required: false });
+        const workItemType = core.getInput('work-item-type', { required: false });
+        const issuePrefix = core.getInput('issue-prefix', { required: false });
+
+        // create GitHub connection
+        const octokit = new github.GitHub(githubPAT);
+
+        const workItems = await getWorkItems(azpPAT, azpOrg, areaPath, workItemType);
+        const existingIssues = await getExistingIssues(octokit, labelFilter, issuePrefix); 
+        createIssues(octokit, existingIssues, workItems, labelForIssues, issuePrefix);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run()
