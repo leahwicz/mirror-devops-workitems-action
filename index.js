0afe5bc932688c201a677898a360ef3dc0947391
Leah Antkiewicz <laa018@bucknell.edu>
2020-05-15 22:04:05 -0400

    Adding logging

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ async function getWorkItems(azpPAT, azpOrg, areaPath, workItemType) {
     const filterOnType = workItemType ? `[System.WorkItemType] = '${workItemType}' AND` : ``
     const escapedPath = areaPath.replace(/[\\"']/g, '\\$&')
     const wiql = `SELECT [System.Id], [System.WorkItemType], [System.Title], [System.State] from workitems where ${filterOnType} [System.AreaPath] UNDER '${escapedPath}' AND [System.State] = '5 - PG Engaged'`;
+    console.log(wiql)
+    
     const result = await nodeApi.queryByWiql({query: wiql});
 
     const count = result['workItems'].length;
